090a6be7121dc9d56cb91771b51a1852c9afa972
dariagruszkiewicz <117391460+dariagruszkiewicz@users.noreply.github.com>
2023-04-11 19:51:31 +0000

    Deploying to gh-pages from @ dariagruszkiewicz/goit-js-hw-09@0f7cfc85c475433b11e4d95f51143306adb24f67 🚀

---
+++ 01-color-switcher.53d866bc.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),o=null;var r=function(){n.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))};t.addEventListener("click",(function(){o=setInterval(r,1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.53d866bc.js.map
---
+++ 01-color-switcher.ae5c292d.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let o=null;const n=()=>{r.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`};t.addEventListener("click",(()=>{o=setInterval(n,1e3),t.setAttribute("disabled","")})),e.addEventListener("click",(()=>{clearInterval(o),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.ae5c292d.js.map
