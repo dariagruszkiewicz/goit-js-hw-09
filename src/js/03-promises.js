07d643ae6cf09833a15af8b896694345ba5e5635
Daria Gruszkiewicz <dariaa.pajak@gmail.com>
2023-04-16 22:10:31 +0200

    update task 03
    update task 03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const formEl = document.querySelector('.form');
+
+const createPromise = (position, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      shouldResolve
+        ? resolve({ position, delay })
+        : reject({ position, delay });
+    }, delay);
+  });
+};
+
+const onSuccess = ({ position, delay }) => {
+  console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+};
+
+const onFail = ({ position, delay }) => {
+  console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+};
+
+const createPromises = ev => {
+  ev.preventDefault();
+
+  let {
+    elements: { delay, step, amount },
+  } = ev.currentTarget;
+
+  let firstDelay = Number(delay.value);
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    if (i > 1) {
+      firstDelay += Number(step.value);
+    }
+    createPromise(i, firstDelay).then(onSuccess).catch(onFail);
   }
-}
+};
+
+formEl.addEventListener('submit', createPromises);
